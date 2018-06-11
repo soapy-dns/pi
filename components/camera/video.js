2029b821b4f55370d9a1701bbbede524739cd349
neilsoutar <neil.soutar@iag.com.au>
2018-06-12 07:49:12 +1000

    init commit

---
+++ components/camera/video.js
@@ -0,0 +1,25 @@
+
+const PiCamera = require('pi-camera')
+
+const _record = async (camera) => {
+        await camera.record()
+    // return Promise.resolve()
+}
+
+setTimeout(function(){
+    // const seconds = 5 * 1000
+    const minutes = 1 * 60 * 1000
+    const myCamera = new PiCamera({
+        mode: 'video',
+        output: `${ __dirname }/video.h264`,
+        width: 1920,
+        height: 1080,
+        timeout: minutes, // Record for 5 seconds
+        nopreview: true,
+    })
+
+    _record(myCamera).then(() => {
+        console.log('video captured')
+    }).catch(err =>  console.log('error capturing video', err)
+    )
+}, 3000)
