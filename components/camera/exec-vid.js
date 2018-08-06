60e6e10282a05121e169ba1f87fb4e4582682e80
soapy-dns <neil.soutar@iag.com.au>
2018-08-07 01:41:39 +1000

    oneOff

--- components/camera/exec-vid.js
+++ components/camera/exec-vid.js
@@ -7,10 +7,25 @@ const fs = require('fs')
 const args = require('yargs').argv
 const moment = require('moment')
 
+
+const _fileExists = async () => {
+    const path = `${__dirname}/vid-oneOff.h264`
+    console.log('path to check', path)
+    // Check if the file exists in the current directory.
+    fs.access(path, fs.constants.F_OK, (err) => {
+        console.log('access check', err)
+        console.log(`${path} ${err ? 'does not exist' : 'exists'}`)
+        if (!err) return false
+        return true
+    })
+}
+
 if (!args.t) return console.log('no time option (-t) set')
 let fileName
 if (args.oneOff) {
     fileName = `${__dirname}/vid-oneOff.h264`
+    console.log('oneOff fileName', fileName)
+    _fileExists(fileName).then().catch(() => (console.log('file exists')))
 } else {
     fileName = `${__dirname}/vid-${moment().format('YYYYMMDDHHmmss')}.h264`
 }
