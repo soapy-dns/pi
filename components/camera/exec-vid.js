1744d7e857a9af114b78d14a441428d83f4e85c9
soapy-dns <neil.soutar@iag.com.au>
2018-08-08 08:30:06 +1000

    oneOff processing

--- components/camera/exec-vid.js
+++ components/camera/exec-vid.js
@@ -4,37 +4,54 @@
  */
 const cp = require('child_process')
 const fs = require('fs')
-const args = require('yargs').argv
+const arguements = require('yargs').argv
 const moment = require('moment')
+const util = require('util')
 
 
-const _fileExists = async () => {
-    const path = `${__dirname}/vid-oneOff.h264`
-    console.log('path to check', path)
-    // Check if the file exists in the current directory.
-    fs.access(path, fs.constants.F_OK, (err) => {
-        console.log('access check', err)
-        console.log(`${path} ${err ? 'does not exist' : 'exists'}`)
-        if (!err) return false
-        return true
+const _fileExists = () => {
+    return new Promise((resolve) => {
+        const path = `${__dirname}/vid-oneOff.h264`
+        console.log('path to check', path)
+        fs.access(path, fs.constants.F_OK, (err) => {
+            console.log('access check', err)
+            console.log(`${path} ${err ? 'does not exist' : 'exists'}`)
+            if (!err) resolve(true)
+
+            console.log('file does not exist')
+            resolve(false)
+        })
     })
 }
 
-if (!args.t) return console.log('no time option (-t) set')
-let fileName
-if (args.oneOff) {
-    fileName = `${__dirname}/vid-oneOff.h264`
-    console.log('oneOff fileName', fileName)
-    _fileExists(fileName).then().catch(() => (console.log('file exists')))
-} else {
-    fileName = `${__dirname}/vid-${moment().format('YYYYMMDDHHmmss')}.h264`
+const _takeVideo = (fileName, time) => {
+    const command = `raspivid -o ${fileName} -t ${time}`
+    console.log('command', command)
+
+    const child = cp.exec(command, (err, child_stdout, child_stderr) => {
+        if (err) return console.log('err', err, child_stderr)
+
+        console.log('stdout', child_stdout)
+    })
 }
 
-const command = `raspivid -o ${fileName} -t ${args.t}`
-console.log('command', command)
+const _doIt = async (args) => {
+    if (!args.t) return console.log('no time option (-t) set')
+    let fileName
+    if (args.oneOff) {
+        fileName = `${__dirname}/vid-oneOff.h264`
+        console.log('oneOff', )
+        const exists = await _fileExists(fileName)
+        console.log('exists', exists)
+        if (!exists) {
+            _takeVideo(fileName, args.t)
+        } else {
+            console.log('file already exists')
+        }
+    } else {
+        fileName = `${__dirname}/vid-${moment().format('YYYYMMDDHHmmss')}.h264`
+        _takeVideo(fileName, args.t)
+    }
+}
 
-const child = cp.exec(command, (err, child_stdout, child_stderr) => {
-    if (err) return console.log('err', err, child_stderr)
-
-    console.log('stdout', child_stdout)
-})
+_doIt(arguements)
